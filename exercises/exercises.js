6af22fee304941dcddd81ffd1aa2144bf9d16688
VinniC77 <vinniclunc@hotmail.com>
2022-08-24 21:46:45 -0300

    Added for

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -17,3 +17,26 @@
 // } else {
 //     console.log("Not elegible, you have already gotten one")
 // }
+
+// for
+// for (let i = 10; i <= 100; i += 10) {
+//     console.log(i)
+// }
+
+// let messages = [
+//     'Hey, how is it going?',
+//     'I am great, thank you! How about you?',
+//     'All good. Been working on my portfolio lately.',
+//     'Same here!',
+//     'Great to hear!'
+// ]
+
+// for (let i = 0; i < messages.length; i++) {
+//     console.log(messages[i]);
+// }
+
+let cards = [7, 3, 6]
+
+for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i]);
+}
