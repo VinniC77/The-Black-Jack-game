43be0c32470e449de963eaebe624ee1f857dcea1
VinniC77 <vinniclunc@hotmail.com>
2022-08-27 02:08:03 -0300

    Add some features like random cards and re-assign some variables

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -35,8 +35,61 @@
 //     console.log(messages[i]);
 // }
 
-let cards = [7, 3, 6]
+// let cards = [7, 3, 6]
 
-for (let i = 0; i < cards.length; i++) {
-    console.log(cards[i]);
-}
+// for (let i = 0; i < cards.length; i++) {
+//     console.log(cards[i]);
+// }
+
+// let sentence = ['Hello', 'my', 'name', 'is', 'Vini']
+// let greetingEl = document.getElementById('greeting-el')
+
+// for (let i = 0; i < sentence.length; i++) {
+//     greetingEl.textContent += sentence[i] + " "
+// }
+
+// function to see what is the less time
+// let player1Time = 102;
+// let player2Time = 107;
+
+// function getFastestTime() {
+//     if (player1Time < player2Time) {
+//         return player1Time
+//     } else if (player2Time < player1Time) {
+//         return player2Time
+//     } else {
+//         return player1Time
+//     }
+// }
+
+// let = fastestRace = getFastestTime();
+
+// function totalRace() {
+//     return player1Time + player2Time
+// }
+
+// let total = totalRace();
+
+// console.log(total);
+
+// let randomNumber = Math.random(); // It gives a number from 0 to 0.99999
+// console.log(randomNumber);
+
+// let dado = Math.random() * 6; // It gives a number from 0 to 5.99999
+// console.log(dado)
+
+// let flooredNumber = Math.floor(3.45632); // Remove os decimais. Arredonda qualuer numero gerado para 3.
+// console.log(flooredNumber);
+
+// let randNumber = Math.floor(Math.random() * 6); // Retorna qualquer um dos números: 0, 1, 2, 3, 4 ou 5.
+// console.log(randNumber);
+
+// let diceRandom = Math.floor(Math.random(1) * 6) + 1; // Retorna qualquer um dos números: 1, 2, 3, 4, 5 ou 6.
+// console.log(diceRandom);
+
+// function rollDice() {
+//     let randDice = Math.floor(Math.random() * 6) + 1;
+//     return randDice;
+// }
+
+// console.log(rollDice());
--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let firsCard = 10;
-let secoundCard = 11;
-let cards = [firsCard, secoundCard];
-let sum = firsCard + secoundCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 
 let messageEl = document.getElementById('message-el');
@@ -11,12 +9,32 @@ let messageEl = document.getElementById('message-el');
 let sumEl = document.querySelector(".sum-el");
 let cardsEl = document.getElementById('cards-el')
 
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() * 13) + 1
+    if (randomCard === 1) {
+        return 11;
+    } else if (randomCard > 10) {
+        return 10;
+    } else {
+        return randomCard;
+    }
+}
+
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     renderGame();
 }
 
 function renderGame() {
-    cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`
+    cardsEl.textContent = "Cards: "
+    for(let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = `Sum: ${sum} `
     if(sum <= 20) {
         message = "Você quer tirar outra carta?"
@@ -32,9 +50,8 @@ function renderGame() {
 }
 
 const newCard = () => {
-    let card = 7;
+    let card = getRandomCard();
     sum += card;
     cards.push(card);
     renderGame();
 }
-
